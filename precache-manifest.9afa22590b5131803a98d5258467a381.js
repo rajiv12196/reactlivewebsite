2b38c36617c350558d037aa70d64ce48007662fb
rajiv12196 <rajeevray12345@gmail.com>
2020-08-30 15:44:45 +0530

    Updates

---
+++ precache-manifest.9afa22590b5131803a98d5258467a381.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdb20ab98405cbbf4c45638841a86ca",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "9aac183f20ef2a87385e",
+    "url": "/reactlivewebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "17d5c36ed75ac40d6407",
+    "url": "/reactlivewebsite/static/css/main.c81c3934.chunk.css"
+  },
+  {
+    "revision": "9aac183f20ef2a87385e",
+    "url": "/reactlivewebsite/static/js/2.824e7ade.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/reactlivewebsite/static/js/2.824e7ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d5c36ed75ac40d6407",
+    "url": "/reactlivewebsite/static/js/main.4d4bf5d5.chunk.js"
+  },
+  {
+    "revision": "479d0dccb2f89142f4fb",
+    "url": "/reactlivewebsite/static/js/runtime-main.2a28c33f.js"
+  },
+  {
+    "revision": "52dcd0a4c5559452b1d3ae33f012b4d1",
+    "url": "/reactlivewebsite/static/media/Tdz.52dcd0a4.png"
+  }
+]);
